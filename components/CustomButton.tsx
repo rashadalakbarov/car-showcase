45e4092c791d77dccaf028eec0f5303ed1a45863
Rashad Alakbarov <rashad.alakbarov.777@gmail.com>
2025-03-24 20:32:42 +0400

    added layout to project

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,17 +1,28 @@
 "use client"
 
 import { CustomButtonProps } from '@/types'
+import Image from 'next/image'
 
-const CustomButton = ({title, containerStyles, handleClick}: CustomButtonProps) => {
+const CustomButton = ({isDisabled, btnType, containerStyles, textStyles, title, rightIcon, handleClick}: CustomButtonProps) => {
   return (
     <button
-        disabled={false}
-        type={'button'}
+        disabled={isDisabled}
+        type={btnType || "button"}
         className={`flex flex-row relative justify-center items-center py-3 px-6 outline-none cursor-pointer ${containerStyles}`}
         onClick={handleClick}>
-        <span className={`flex-1`}>
+        <span className={`flex-1 ${textStyles}`}>
             {title}
         </span>
+        {rightIcon && (
+            <div className="relative w-6 h-6">
+                <Image
+                    src={rightIcon}
+                    alt="arrow_left"
+                    fill
+                    className="object-contain"
+                />
+            </div>
+        )}
     </button>
   )
 }
